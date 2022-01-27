021da99150d1d4a19a4b25660903d072fc42a04e
Farbod Nowzad <farbod@Farbods-MacBook-Pro.local>
2022-01-26 20:23:15 -0800

    fix search icon

--- js/home.js
+++ js/home.js
@@ -8,16 +8,15 @@ async function get_feed() {
     // Storing response
     await $.get(url, function(data){
         // Display the returned data in browser
-        console.log(data)
         return show(data)
     });
 }
 get_feed();
-$('.feed-campaign').click(function(e){
-    if(e.target.nodeName == 'A') return;
-    window.location = $(this).find("a").attr("href");
-    return false;
-});
+// $('.feed-campaign').click(function(e){
+//     if(e.target.nodeName == 'A') return;
+//     window.location = $(this).find("a").attr("href");
+//     return false;
+// });
 function show(data) {
     var results = data.campaigns
     let row = ``;
@@ -26,8 +25,7 @@ function show(data) {
         var budget_formatted = internationalNumberFormat.format(campaign.budget / 100)
         var requires_approval = campaign.requires_approval ? "<div class='requires-approval'><img class = 'requirement-icon' src='../assets/img/requires_approval_icon.png'/> Requires Approval</div>" : ""
         var sends_product = campaign.requires_product ? "<div class='sends-product'><img class = 'requirement-icon' src='../assets/img/sends_product_icon.png'/> Sends Product</div>" : ""
-        row += `<div class="feed-campaign">
-                    <a href="campaign.html"></a>
+        row += `<div class="feed-campaign" campaign_id=${campaign._id}>
                     <div class="content-container">
                         <div class="banner-image">
                             <img src="${campaign.primary_image}"/>
@@ -37,7 +35,7 @@ function show(data) {
                             <div class="description"><span class="bold">Scalelink Summer Launch</span></div>
                             <div class="description">${campaign.about} everybody know its going to be an epic summer so come have fun with it</div>
                             <div class="url">
-                                <a href="http://${campaign.url}">${campaign.url}</a>
+                                <a href="http://${campaign.url}" target="_blank">${campaign.url}</a>
                             </div>
                         </div>
                         <div class="requirements">
@@ -53,4 +51,7 @@ function show(data) {
     }
     // Setting innerHTML as tab variable
     document.getElementById("campaign-feed").innerHTML = row;
+    $(document).on("click", ".feed-campaign", function() {
+        window.location.href = `campaign.html?id=${this.getAttribute("campaign_id")}`
+    })
 }
