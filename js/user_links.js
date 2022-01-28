0c3058a3b6deb3d8af628e6c197f4bfbfd7f0439
Farbod Nowzad <farbod@Farbods-MacBook-Pro.local>
2022-01-28 08:27:13 -0800

    update links styling in feed

--- js/user_links.js
+++ js/user_links.js
@@ -21,61 +21,41 @@ function show(data) {
         var business = businesses.filter(business => business._id == [campaign.business_id])[0]
         row += `<div class="feed-campaign">
                     <div class="content-container">
-                    <div class="banner-image">
-                        <img src="${campaign.primary_image}"/>
-                    </div>
-                    <div class="text-content">
-                        <div class="brand-name feed-h1">${business.name}</div>
-                        <div class="link-campaign-description">
-                            <div class="description"><span class="bold">${campaign.title}</span></div>
-                            <a class="link-campaign-url" href="campaign.html?id=${campaign._id}">View Campaign &rarr;</a>
+                        <div class="banner-image">
+                            <img src="${campaign.primary_image}"/>
                         </div>
-                        <div>Expires: ${campaign.expiration}</div>
-                        <div class="link-metrics-row">
-                            <div class="link-metrics-narrow">
-                                <div class="link-metrics-value">
-                                    <span>${link.payout}</span>
+                        <div class="text-content">
+                            <div class="brand-name feed-h1">${business.name}</div>
+                            <div class="link-campaign-description">
+                                <div class="description"><span class="bold">${campaign.title}</span></div>
+                                <a class="link-campaign-url" href="campaign.html?id=${campaign._id}">View Campaign &rarr;</a>
+                            </div>
+                            <div>Expires: ${campaign.expiration}</div>
+                            <div class="link-metrics-row">
+                                <div class="link-metrics-narrow">
+                                    <div class="link-metrics-value">
+                                        <span>$${link.payout}</span>
+                                    </div>
+                                    <div class="link-metrics-title">
+                                        Earnings
+                                    </div>
                                 </div>
-                                <div class="link-metrics-title">
-                                    Earnings
+                                <div class="link-metrics-narrow right-side">
+                                    <div class="link-metrics-value">
+                                        <span>${link.unique_visitors}</span>
+                                    </div>
+                                    <div class="link-metrics-title">
+                                        Unique Visitors
+                                    </div>
                                 </div>
                             </div>
-                            <div class="link-metrics-narrow right-side">
-                                <div class="link-metrics-value">
-                                    <span>${link.unique_visitors}</span>
-                                </div>
-                                <div class="link-metrics-title">
-                                    Unique Visitors
-                                </div>
+                            <div class="main-action-button" id="main-action-button">
+                                <img class="icon" src="../assets/img/copy_link_icon.png"/><span id="main-action-button-text" class="copy-link-text"> Copy Link</span>
                             </div>
-                        </div>
-                        <div class="main-action-button" id="main-action-button">
-                            <img class="icon" src="../assets/img/copy_link_icon.png"/><span id="main-action-button-text" class="copy-link-text"> Copy Link</span>
                         </div>
                     </div>
                 </div>`;
-        // row += `<div class="feed-campaign">
-        //             <div class="content-container">
-        //                 <div class="banner-image">
-        //                     <img src="${campaign.primary_image}"/>
-        //                 </div>
-        //                 <div class="text-content">
-        //                     <div class="brand-name feed-h1">${business.name}</div>
-        //                     <div class="link-campaign-description">
-        //                         <div class="description"><span class="bold">${campaign.title}</span></div>
-        //                         <a class="link-campaign-url" href="campaign.html?id=${campaign._id}">View Campaign &rarr;</a>
-        //                     </div>
-        //                     <div class="timestamp">Expires: ${campaign.expiration}</div>
-        //                 </div>
-        //                 <div class="text-content">
-        //                     <div class="description">Status: ${campaign.status}</div>
-        //                 </div>
-        //             </div>
-        //         </div>`;
     }
     // Setting innerHTML as tab variable
     document.getElementById("links-feed").innerHTML = row;
-    // $(document).on("click", ".feed-campaign", function() {
-    //     window.location.href = `campaign.html?id=${this.getAttribute("campaign_id")}`
-    // })
 }
