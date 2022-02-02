b472d3f9fa7c2fa5f77ea7e5534eed39fe4425c9
Farbod Nowzad <farbod@Farbods-MacBook-Pro.local>
2022-02-02 12:56:11 -0800

    nit

--- js/home.js
+++ js/home.js
@@ -78,7 +78,7 @@ function show(data) {
         window.location.href = `campaign.html?id=${this.getAttribute("campaign_id")}`
     })
     $(document).on("click", ".connect-instagram", function() {
-        var url = `https://api.instagram.com/oauth/authorize?client_id=1130340001160455&redirect_uri=https://.scalelink.xyz/app/auth.html?user_id=${user_id}&scope=user_profile&response_type=code`
+        var url = `https://api.instagram.com/oauth/authorize?client_id=1130340001160455&redirect_uri=https://scalelink.xyz/app/auth.html?user_id=${user_id}&scope=user_profile&response_type=code`
         window.open(url, '_blank');
     })
 }
